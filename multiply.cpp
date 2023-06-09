a42b698f02cc1ec10d2aa9e2e0e7c8e545ace51c
Aman <123ramramji456@gmail.com>
2023-06-09 22:18:19 +0530

    multiply component is at initial stage

---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    int a, b;
+    std::cout<<"Enter two values : "<<endl;
+    std::cin>>a>>b;
+    std::cout<<"Multiply of a and b is : "<<a*b;
+    
+    return 0;
+}
